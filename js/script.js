fb15ebcadbc5e9f9b1b8c5242275ff38277d0992
roshen1234 <roshankoshy123@gmail.com>
2023-12-27 18:29:38 +0530

    final

---
+++ js/script.js
@@ -0,0 +1,78 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+// let header=document.querySelector('.header')
+
+// header.addEventListener('click',function(e){
+// let id=e.target.dataset.id
+// if(id)
+// {
+//     navLinks.forEach(link =>{
+//         link.classList.remove('active')
+//     })
+//     e.target.classList.add('active')
+// }
+// })
+// or the comand written insite the window.onscroll
+ 
+
+
+
+// window.addEventListener("scroll",function(){
+    // )}
+    // or
+window.onscroll = () => {
+   
+   
+//    active navbarlink
+
+sections.forEach(sec=>{
+    let top=window.scrollY;
+    let offset=sec.offsetTop-100;
+    let height=sec.offsetHeight;
+    let id=sec.getAttribute('id');
+    if(top>=offset && top<offset+height)
+    {
+        navLinks.forEach(link=>{
+            link.classList.remove('active')
+            document.querySelector('header nav a[href*='+id+']').classList.add('active');
+        })
+        //active section for animate on scroll
+        sec.classList.add('show-animate');
+    }
+    else{
+        // if want to use animate the rrpeat on scroll use this
+        sec.classList.remove('show-animate');
+    }
+})
+   
+   
+    // sticky header
+   let header=document.querySelector('header');
+   header.classList.toggle('sticky',window.scrollY>100)
+
+
+    
+// remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove('bx-x')
+  navbar.classList.remove('active')
+    
+  // animation footer on scroll
+   let footer =document.querySelector('footer');
+   footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+
+
+}
+
+
